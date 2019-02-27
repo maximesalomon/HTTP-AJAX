b530ecf4a4a55608548f537718c2e8536e793cde
Maxime Salomon <maxime@croissant.io>
2019-02-27 15:08:29 +0100

    MVP

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    friends: [],
+    error: null,
+    loading: false
+  }
+
+  componentDidMount() {
+    this.fetchFriends();
+  }
+
+  fetchFriends = () => {
+    axios.get('http://localhost:5000/friends')
+      .then(res => this.setFriends(res.data))
+  }
+
+  setFriends = friends => {
+    this.setState({ friends })
+  }
+
+
   render() {
     return (
       <div>
-        <header>
-          <p>Hello World!</p>
-        </header>
+        <h2>Add a new friend</h2>
+        <div>Name: <input></input></div>
+        <div>Age: <input></input></div>
+        <div>Email: <input></input></div>
+        <button>Add friend</button>
+        <br></br>
+        <h2>Friends List</h2>
+        {
+          this.state.friends.map(friend =>
+            <div>
+              <p><span>Name:</span> {friend.name}<span> | Age:</span> {friend.age} | <span>Email:</span> {friend.email}</p>
+            </div>
+          )
+        }
       </div>
     );
   }
